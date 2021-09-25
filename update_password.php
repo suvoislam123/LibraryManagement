00f9fcd141a7916c1d8f66338e30e70b40b688ef
suvoislam123 <suvoislam753@gmail.com>
2021-09-26 03:39:47 +0600

    eto change!

--- update_password.php
+++ update_password.php
@@ -2,6 +2,7 @@
 require 'includes/db-inc.php';
 session_start();
 $student_name = $_SESSION['student-username'];
+$password = $_POST['password'];
 $cpassword = $_POST["cpassword"];
 $sql = "Update students SET password = '{$cpassword}' where username = '$student_name'";
 $query = mysqli_query($conn, $sql);
